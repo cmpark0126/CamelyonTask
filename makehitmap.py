6f528cf77c141892cd8d36a8b465eaf8c254f84b
EngineerJeYun <jjy8464@gmail.com>
2018-02-07 23:50:02 +0900

    fix comflict

--- makehitmap.py
+++ makehitmap.py
@@ -9,13 +9,13 @@ import openslide
 import cv2
 
 
-slide_fn = 'b_13'
+slide_fn = 'b_1'
 output_level = 4
 
-f = open(cf.path_for_generated_image + "/result.csv",
+f = open(cf.path_for_generated_image + "/" + slide_fn + "_result.csv",
          'r', encoding='utf-8')
 
-target_path = os.path.join(cf.path_of_task_1, slide_fn + ".tif")
+target_path = os.path.join(cf.path_of_slide, slide_fn + ".tif")
 slide = openslide.OpenSlide(target_path)
 
 print("start")
@@ -24,7 +24,8 @@ output = np.zeros(shape=slide.level_dimensions[output_level][::-1])
 
 rdr = csv.reader(f)
 for line in rdr:
-    #print(line)
+    print(line)
+    
     if line[2] == '1.0':
         print(line[0], line[1])
         x_pos = line[0].strip()
@@ -32,6 +33,6 @@ for line in rdr:
         y_pos = line[1].strip()
         y = round(int(y_pos)/slide.level_downsamples[output_level])
         output[y:y+19, x:x+19] = 255
-
+    
 
 cv2.imwrite(slide_fn + '_reult.png', output)
